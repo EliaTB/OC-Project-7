9ebad06398f1881f95b79a638207611a18229bac
Elia <beneli.t@icloud.com>
2018-09-13 23:46:48 +0200

    test apiwiki update

--- grandpy/test_apiwiki.py
+++ grandpy/test_apiwiki.py
@@ -1,14 +1,18 @@
 import pytest
 from apiwiki import *
 
-wikiresult = Wiki()
+wiki = Wiki()
+question = 'paris'
 
 class TestApiWiki:
 
-	def test_wiki_result(self):
-		question = 'paris'
-		print(wikiresult.get_wiki_result(question))
-		wiki_p = wikiresult.get_wiki_result(question)
+	def test_wiki_title(self):
+		wiki_p = wiki.get_wiki_result(question)
+		assert wiki_p["wiki_title"] == "Paris"
+
+	def test_wiki_summary(self):
+		print(wiki.get_wiki_result(question))
+		wiki_p = wiki.get_wiki_result(question)
 		check_summary = 0
 		string_test = "est la capitale de la France."
 		if string_test in wiki_p["wiki_summary"]:
