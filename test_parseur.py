ac32d0b6265e73e5c695a3d52851db6dab348713
EliaTB <34030726+EliaTB@users.noreply.github.com>
2018-09-13 14:26:19 +0200

    Implémentation  parseur

---
+++ test_parseur.py
@@ -0,0 +1,14 @@
+import pytest
+
+from parseur import *
+from stop_word import STOP_WORDS
+
+
+parseur = Parseur(CUSTOM_WORDS, STOP_WORDS)
+
+class TestParseur:
+
+	def test_parse_word(self):
+		question = "Salut GrandPyBoT, Est-ce que tu peux me parler de Paris?"
+		assert parseur.get_relevant_word(question) == "paris"
+
