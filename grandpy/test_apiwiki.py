fb4bbcc5e732f05bba99b3b847f31729d35f9870
Elia <beneli.t@icloud.com>
2018-09-14 10:11:23 +0200

    quick wikiapi update

--- grandpy/test_apiwiki.py
+++ grandpy/test_apiwiki.py
@@ -3,18 +3,16 @@ from apiwiki import *
 
 wiki = Wiki()
 question = 'paris'
+wiki_result = wiki.get_wiki_result(question)
 
 class TestApiWiki:
 
 	def test_wiki_title(self):
-		wiki_p = wiki.get_wiki_result(question)
-		assert wiki_p["title"] == "Paris"
+		assert wiki_result["title"] == "Paris"
 
 	def test_wiki_summary(self):
-		print(wiki.get_wiki_result(question))
-		wiki_p = wiki.get_wiki_result(question)
 		check_summary = 0
 		string_test = "est la capitale de la France."
-		if string_test in wiki_p["summary"]:
+		if string_test in wiki_result["summary"]:
 			check_string = 1
 		assert check_string == 1
